13705376ff52edd9b0d3a1e04670043270de3ccd
Jan Marek <mail@janmarek.net>
2010-11-16 22:01:54 +0100

    menu item test skeleton

---
+++ tests/Model/Menu/MenuItemTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Neuron\Model;
+
+class MenuItemTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var MenuItem
+	 */
+	protected $object;
+
+
+
+	protected function setUp()
+	{
+		$this->object = new MenuItem;
+	}
+
+
+
+	protected function tearDown()
+	{
+
+	}
+
+
+
+	public function testGetUrl()
+	{
+		$this->markTestIncomplete();
+	}
+
+
+
+	public function testSetUrl()
+	{
+		$this->markTestIncomplete();
+	}
+
+}
