07cebba6b2af873f8ece381f787813c5a7f79120
Jan Marek <mail@janmarek.net>
2010-12-14 15:41:51 +0100

    generate model generate tests

--- cli/GenerateModel/GenerateModel.php
+++ cli/GenerateModel/GenerateModel.php
@@ -31,7 +31,9 @@ class GenerateModel extends AbstractCommand
 	{
 		$name = ucfirst($input->getOption("name"));
 		$dir = $input->getOption("output") ?: APP_DIR . "/Model/" . $name;
+		$testDir = WWW_DIR . "/tests/Model/$name";
 		$namespace = ucfirst($input->getOption("application")) . "\\Model\\" . $name;
+		$testNamespace = ucfirst($input->getOption("application")) . '\Tests\Model\\' . $name;
 
 		// entity
 		$template = $this->createTemplate(__DIR__ . "/entity.phtml");
@@ -39,17 +41,38 @@ class GenerateModel extends AbstractCommand
 		$template->namespace = $namespace;
 		$this->tryWriteFile("$dir/$name.php", $template, $output);
 
+		// entity test
+		$template = $this->createTemplate(__DIR__ . "/phpunit.entity.phtml");
+		$template->name = $name;
+		$template->namespace = $namespace;
+		$template->testNamespace = $testNamespace;
+		$this->tryWriteFile($testDir . "/" . $name . "Test.php", $template, $output);
+
 		// service
 		$template = $this->createTemplate(__DIR__ . "/service.phtml");
 		$template->name = $name;
 		$template->namespace = $namespace;
 		$this->tryWriteFile("$dir/Service.php", $template, $output);
 
+		// service test
+		$template = $this->createTemplate(__DIR__ . "/phpunit.service.phtml");
+		$template->name = $name;
+		$template->namespace = $namespace;
+		$template->testNamespace = $testNamespace;
+		$this->tryWriteFile("$testDir/ServiceTest.php", $template, $output);
+
 		// finder
 		$template = $this->createTemplate(__DIR__ . "/finder.phtml");
 		$template->name = $name;
 		$template->namespace = $namespace;
 		$this->tryWriteFile("$dir/Finder.php", $template, $output);
+
+		// finder test
+		$template = $this->createTemplate(__DIR__ . "/phpunit.finder.phtml");
+		$template->name = $name;
+		$template->namespace = $namespace;
+		$template->testNamespace = $testNamespace;
+		$this->tryWriteFile("$testDir/FinderTest.php", $template, $output);
 	}
 
 }
